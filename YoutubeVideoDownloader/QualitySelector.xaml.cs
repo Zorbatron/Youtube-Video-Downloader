0bda5ec4fa70d69dc494d731e07d04d932fb992e
Gabriel Sakash <gabrielsakash@gmail.com>
2021-06-05 09:35:04 -0400

    Quality selector code functioning, fetching quality data functioning

--- YoutubeVideoDownloader/QualitySelector.xaml.cs
+++ YoutubeVideoDownloader/QualitySelector.xaml.cs
@@ -2,6 +2,7 @@
 using System.Windows;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Windows.Controls;
 
 namespace YoutubeVideoDownloader
 {
@@ -24,7 +25,7 @@ namespace YoutubeVideoDownloader
             string newLine = Environment.NewLine;
 
             List<string> output = new List<string>();
-            List<string> qualNum = new List<string>();
+            List<int> qualNum = new List<int>();
             List<string> qualType = new List<string>();
             List<string> qualResolution = new List<string>();
 
@@ -32,32 +33,51 @@ namespace YoutubeVideoDownloader
             {
                 output.Add(p.StandardOutput.ReadLine());
             }
-
+            
             output.RemoveRange(0, 3);
 
             foreach (string text in output)
             {
-                qualNum.Add(text.Remove(3));
+                qualNum.Add(Convert.ToInt32(text.Remove(3)));
             }
 
             foreach (string text in output)
             {
-                text.Remove(0, 13);
-                qualType.Add(text.Remove(3));
+                string temp = text.Remove(0, 13);
+                temp = temp.Remove(3);
+                qualType.Add(temp);
             }
 
             foreach (string text in output)
             {
-                text.Remove(0, 24);
-                qualResolution.Add(text.Remove(10));
+                string temp = text.Remove(0, 24);
+                temp = temp.Remove(10);
+                qualResolution.Add(temp);
             }
 
             for (int x = 0; x < output.Count; x++)
-            {
+            { 
                 OutputBox.Text += qualNum[x] + " ";
                 OutputBox.Text += qualType[x] + " ";
                 OutputBox.Text += qualResolution[x] + newLine;
             }
+
+            for (int x = 0; x < output.Count; x++)
+            {
+                Button button = new Button();
+                button.Name = $"BT{qualNum[x]}";
+                button.Content = qualResolution[x];
+                button.VerticalAlignment = VerticalAlignment.Center;
+                button.Width = 100;
+                button.Click += new RoutedEventHandler(QualButtonPressed);
+                StackPanel.Children.Add(button);
+            }
+        }
+
+        private void QualButtonPressed(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            MessageBox.Show($"Resolution: {button.Content}", $"Button pressed: {button.Name}");
         }
     }
 }
